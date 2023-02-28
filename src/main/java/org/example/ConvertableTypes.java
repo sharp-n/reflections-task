5ea29d9e78dc1bb8709f7da68fc3501e0f3efee3
YANA <sof8013@gmail.com>
2023-02-28 13:23:50 +0200

    Refactor hierarchy for primitive types

--- src/main/java/org/example/ConvertableTypes.java
+++ src/main/java/org/example/ConvertableTypes.java
@@ -20,22 +20,48 @@ public class ConvertableTypes {
             byte.class,short.class,char.class,int.class,long.class,float.class,double.class
     ));
 
+
+    static final List<Class<?>> primitivesForByte = new ArrayList<>(Arrays.asList(
+            byte.class,short.class,char.class,int.class,long.class,float.class,double.class
+    ));
+
+    static final List<Class<?>> primitivesForShort = new ArrayList<>(Arrays.asList(
+            short.class,int.class,long.class,float.class,double.class,char.class,byte.class
+    ));
+
+    static final List<Class<?>> primitivesForChar = new ArrayList<>(Arrays.asList(
+           char.class,int.class,long.class,float.class,double.class,short.class,byte.class
+    ));
+
+    static final List<Class<?>> primitivesForInt = new ArrayList<>(Arrays.asList(
+            int.class,long.class,float.class,double.class,char.class,short.class,byte.class
+    ));
+
+    static final List<Class<?>> primitivesForLong = new ArrayList<>(Arrays.asList(
+            long.class,float.class,double.class,int.class,char.class,short.class,byte.class
+    ));
+
+    static final List<Class<?>> primitivesForFloat = new ArrayList<>(Arrays.asList(
+            float.class,double.class,long.class,int.class,char.class,short.class,byte.class
+    ));
+
     static Map<Class<?>, List<Class<?>>> convertableTypes = new HashMap<>();
 
     static {
-        convertableTypes.put(byte.class,concatLists(baseHierarchyForByte,primitives));
-        convertableTypes.put(short.class,concatLists(baseHierarchyForShort,primitives));
-        convertableTypes.put(char.class,concatLists(baseHierarchyForChar,primitives));
-        convertableTypes.put(int.class,concatLists(baseHierarchyForInt,primitives));
-        convertableTypes.put(long.class,concatLists(baseHierarchyForLong,primitives));
-        convertableTypes.put(float.class,concatLists(baseHierarchyForFloat,primitives));
+        convertableTypes.put(byte.class,concatLists(baseHierarchyForByte,primitivesForByte));
+        convertableTypes.put(short.class,concatLists(baseHierarchyForShort,primitivesForShort));
+        convertableTypes.put(char.class,concatLists(baseHierarchyForChar,primitivesForChar));
+        convertableTypes.put(int.class,concatLists(baseHierarchyForInt,primitivesForInt));
+        convertableTypes.put(long.class,concatLists(baseHierarchyForLong,primitivesForLong));
+        convertableTypes.put(float.class,concatLists(baseHierarchyForFloat,primitivesForFloat));
         convertableTypes.put(Integer.class, baseHierarchyForInt);
         convertableTypes.put(Byte.class,baseHierarchyForByte);
         convertableTypes.put(Short.class,baseHierarchyForShort);
         convertableTypes.put(Character.class,baseHierarchyForChar);
         convertableTypes.put(Long.class, baseHierarchyForLong);
         convertableTypes.put(Float.class, baseHierarchyForFloat);
-        convertableTypes.put(Object.class,allTypesList);
+        convertableTypes.put(Number.class, Collections.singletonList(Object.class));
+//        convertableTypes.put(Object.class, null);
     }
 
     @SafeVarargs
