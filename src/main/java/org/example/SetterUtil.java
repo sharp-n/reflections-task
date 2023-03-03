1305ade63c44a104dfe66336f7e726adc1889aa3
YANA <sof8013@gmail.com>
2023-03-03 11:32:13 +0200

    Create interface for setters searching and invoking

---
+++ src/main/java/org/example/SetterUtil.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public interface SetterUtil {
+
+    boolean setValue(boolean value, Object targetObject, String propertyName);
+
+    boolean setValue(byte value, Object targetObject, String propertyName);
+
+    boolean setValue(short value, Object targetObject, String propertyName);
+
+    boolean setValue(char value, Object targetObject, String propertyName);
+
+    boolean setValue(int value, Object targetObject, String propertyName);
+
+    boolean setValue(long value, Object targetObject, String propertyName);
+
+    boolean setValue(float value, Object targetObject, String propertyName);
+
+    boolean setValue(double value, Object targetObject, String propertyName);
+
+    boolean setValue(Object value, Object targetObject, String propertyName);
+
+    boolean setValue(Object value, Object targetObject, String propertyName, Class<?> valueType);
+
+}
